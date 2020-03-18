433de09a69af22dee651681c02fbf31ad295e9a0
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-03-18 09:53:04 +0100

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ from pathlib import Path
 
 short_description = "No description has been added so far."
 
-version = "0.3.0"
+version = "0.3.1"
 
 if (Path().parent / "PACKAGE.rst").is_file():
     with open(str(Path().parent / "PACKAGE.rst")) as readme_file:
         long_description = readme_file.read()
 
-requirements = ["kerasltisubmission>=0.3.0"]
+requirements = ["kerasltisubmission>=0.3.1"]
 test_requirements = [
     "tox",
     "pytest",

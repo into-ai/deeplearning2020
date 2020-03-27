89ab0f25dedc8dd1ae42ea7a5cea52df9b4d09df
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-03-27 18:20:44 +0100

    Bump version: 0.4.13 → 0.4.14

--- deeplearning2020/__init__.py
+++ deeplearning2020/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """into-ai"""
 __email__ = "introintoai@gmail.com"
-__version__ = "0.4.13"
+__version__ = "0.4.14"
 
 from deeplearning2020.submission import Submission as _Submission
 
